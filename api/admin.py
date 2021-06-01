bdee0d222eaed43e2267a448f042eff0a1b14ff9
Maksim Maslennikov <lurkingyuggoth@gmail.com>
2021-06-01 18:59:29 +0300

    models/admin part 2

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+
+from.models import Category, Genre, Title
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    empty_value_display = 'Пустое значение'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    empty_value_display = 'Пустое значение'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'genre', 'category')
+    search_fields = ('name', 'year')
+    list_filter = ('year', 'genre', 'category')
+    empty_value_display = 'Пустое значение'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
--- api/models.py
+++ api/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+
+# User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.CharField(max_length=4)
+    description = models.TextField(blank=True, null=True)
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='titles'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='titles'
+    )
+
+    def __str__(self):
+        return self.name
