f04e1cd22b9dd5e5ec23f1166e3d9d52dfc21b12
Борис <surgeon12111@yandex.ru>
2021-06-02 13:23:30 +0300

    add models Review, Comment

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+# from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # User = get_user_model()
@@ -33,3 +35,34 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    title = models.ForeignKey(
+        'Title', on_delete=models.CASCADE, related_name='reviews'
+    )
+    score = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review = models.ForeignKey(
+        'Review', on_delete=models.CASCADE, related_name='comments'
+    )
+
+    def __str__(self):
+        return self.text
