418e6b5c13932a52055d14eac1314f7eca65f050
Maksim Maslennikov <lurkingyuggoth@gmail.com>
2021-06-05 18:43:36 +0300

    pytest is passed

--- api/models.py
+++ api/models.py
@@ -21,7 +21,6 @@ class Genre(models.Model):
 
 class Title(models.Model):
     name = models.CharField(max_length=200)
-    # year = models.DecimalField(max_digits=4, decimal_places=0)
     year = models.IntegerField()
     description = models.TextField(blank=True, null=True)
     genre = models.ManyToManyField(
@@ -29,8 +28,10 @@ class Title(models.Model):
     )
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True,
+        blank=True,
         related_name='titles'
-    ) # blank=True,
+    )
 
     def __str__(self):
         return self.name
+
--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
-# from django.shortcuts import get_object_or_404
+
 from rest_framework import filters, viewsets
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, DestroyModelMixin
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Category, Genre, Title
 from .permissions import IsUserIsAdmin
@@ -21,9 +20,9 @@ class CustomMixin(ListModelMixin,
 
 class CategoryViewSet(CustomMixin):
     queryset = Category.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly] # , IsUserIsAdmin
+    permission_classes = [IsAuthenticatedOrReadOnly, IsUserIsAdmin]
     serializer_class = CategorySerializer
-    lookup_field = 'slug' # - NB 1111
+    lookup_field = 'slug'
     pagination_class = PageNumberPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['name',]
@@ -31,7 +30,7 @@ class CategoryViewSet(CustomMixin):
 
 class GenreViewSet(CustomMixin):
     queryset = Genre.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly] # , IsUserIsAdmin
+    permission_classes = [IsAuthenticatedOrReadOnly, IsUserIsAdmin]
     serializer_class = GenreSerializer
     lookup_field = 'slug'
     pagination_class = PageNumberPagination
@@ -41,12 +40,12 @@ class GenreViewSet(CustomMixin):
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly] # , IsUserIsAdmin
+    permission_classes = [IsAuthenticatedOrReadOnly, IsUserIsAdmin]
     pagination_class = PageNumberPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = TitleFilter
 
     def get_serializer_class(self):
-        if self.action == ('create' or 'partial_update'):
+        if self.action in ('create', 'partial_update'):
             return TitleCreateSerializer
         return TitleGetSerializer
